efb80341b7966ad680a5e1166721bef49e5152bf
Mauricio Trajano <mauriciot1993@gmail.com>
2017-05-25 16:48:45 -0400

    sample first name search

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,31 +1,70 @@
 <!DOCTYPE html>
 <html>
     <header>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.min.js"></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <script
-          src="https://code.jquery.com/jquery-3.2.1.min.js"
-          integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-          crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue-resource@1.3.3"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     </header>
 
     <body>
-        <div class="container-fluid" >
+        <div id="reorgApp" class="container-fluid">
             <div class="page-header">
               <h1>Reorg Portal</h1>
             </div>
 
-            <div class="row">
-                <div class="col-md-3">
+            <div class="row form-group">
+                <div class="col-sm-3">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Physician First Name" />
+                        <input type="text" class="form-control" placeholder="Physician First Name" v-model="first_name"/>
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">Go!</button>
+                            <button class="btn btn-default" type="button" @click="search">Search</button>
                         </span>
                     </div>
                 </div>
             </div>
+
+            <div class="row" v-if="results">
+                <div class="col-lg-12">
+                    <table class="table table-hover">
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Field</th>
+                            <th>State</th>
+                            <th>Date of Payment</th>
+                            <th>Amount</th>
+                        </tr>
+
+                        <tr v-for="physician in results">
+                            <td>@{{physician.Physician_First_Name}}</td>
+                            <td>@{{physician.Physician_Last_Name}}</td>
+                            <td>@{{physician.Physician_Specialty}}</td>
+                            <td>@{{physician.Physician_License_State_code1}}</td>
+                            <td>@{{physician.Date_of_Payment}}</td>
+                            <td>@{{physician.Total_Amount_of_Payment_USDollars}}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     </body>
+
+    <script>
+        var app = new Vue({
+            el: '#reorgApp',
+            data: {
+                results: null,
+                first_name: null
+            },
+            methods: {
+                search: function() {
+                    this.$http.get('/search?first_name=' + this.first_name).then(response => {
+                        this.results = response.body;
+                    });
+                }
+            }
+        });
+    </script>
 </html>
