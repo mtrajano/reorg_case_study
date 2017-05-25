59357b60ef8d14820d1dfe9400e02bbe84b5c106
Mauricio Trajano <mauriciot1993@gmail.com>
2017-05-25 15:22:12 -0400

    init commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.min.js"
+          integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+          crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </header>
+
+    <body>
+        <div class="container-fluid" >
+            <div class="page-header">
+              <h1>Reorg Portal</h1>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Physician First Name" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
